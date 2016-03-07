dac0333f17b84666a070f64db3fca87fb228f81a
villarrobledocom <juancarlossb@villarrobledojoven.com>
2016-03-07 13:05:03 +0100

    46

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 function alertDismissed() {}
 function onDeviceReady() {
-  alert("Mierda pa tí");
-  navigator.notification.alert("El dispositivo está listo", alertDismissed(), 'Aviso', 'Cerrar');
+  //navigator.notification.alert("El dispositivo está listo", alertDismissed(), 'Aviso', 'Cerrar');
   $('#principal').append('<a href="inicio.html" data-role="button">A la página de inicio...</a>');
 }
 function init() {
