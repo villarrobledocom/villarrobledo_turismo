72f02e135e9225be8be5fa7b885b38f138461837
villarrobledocom <juancarlossb@villarrobledojoven.com>
2016-03-07 12:26:14 +0100

    El nuevo js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 function init() {
-  document.addEventListener("deviceready", onDeviceReady, false);
+  document.addEventListener("deviceready", onDeviceReady(), false);
 }
 function onDeviceReady() {
-  navigator.notification.alert("El dispositivo está listo", alertDismissed, 'Aviso', 'Cerrar');
+  navigator.notification.alert("El dispositivo está listo", alertDismissed(), 'Aviso', 'Cerrar');
   $('#principal').append('<a href="inicio.html" data-role="button">A la página de inicio...</a>');
 }
 function alertDismissed() {}
