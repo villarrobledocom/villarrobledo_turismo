19870cb620839d02deff8cc77d3c989ddb8ae846
villarrobledocom <juancarlossb@villarrobledojoven.com>
2016-03-07 12:59:00 +0100

    44

--- js/index.js
+++ js/index.js
@@ -1,30 +1,9 @@
-/*
-function init() {
-  document.addEventListener("deviceready", onDeviceReady(), false);
-}
+function alertDismissed() {}
 function onDeviceReady() {
   alert("Mierda pa tí");
   navigator.notification.alert("El dispositivo está listo", alertDismissed(), 'Aviso', 'Cerrar');
   $('#principal').append('<a href="inicio.html" data-role="button">A la página de inicio...</a>');
 }
-function alertDismissed() {}
-*/
-
-var app = {
-    initialize: function() {
-        this.bindEvents();
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    receivedEvent: function() {
-      navigator.notification.alert("El dispositivo está listo", alertDismissed, 'Aviso', 'Cerrar');
-      $('#principal').append('<a href="inicio.html" data-role="button">A la página de inicio...</a>');
-    },
-    alertDismissed: function() {
-      // Algo aquí
-    }
-};
+function init() {
+  document.addEventListener("deviceready", onDeviceReady(), false);
+}
