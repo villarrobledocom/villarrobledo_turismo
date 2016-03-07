ddae1a7534a21281bc5f4703090251fad0d2a98d
villarrobledocom <juancarlossb@villarrobledojoven.com>
2016-03-07 13:18:21 +0100

    47

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 function alertDismissed() {}
+function cargaNoticias() {
+  $("#noticias").load("http://www.villarrobledo.com/app/listado_noticias.php");
+}
 function onDeviceReady() {
   //navigator.notification.alert("El dispositivo está listo", alertDismissed(), 'Aviso', 'Cerrar');
   $('#principal').append('<a href="inicio.html" data-role="button">A la página de inicio...</a>');
